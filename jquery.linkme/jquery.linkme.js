acc5810c4d9285837ff1e2d159ce6e18a98db82d
Ben Stein <bumstain@gmail.com>
2016-01-23 19:40:57 +0000

    Created the main plugin file

---
+++ jquery.linkme/jquery.linkme.js
@@ -0,0 +1,38 @@
+ /*
+	* name: LinkMe
+	* author: Ben Stein
+	* version: 0.1.0
+	*license: MIT
+	*/
+
+	(function($) {
+		$.fn.linkMe = function(options) {
+			// Default settings
+			var settings = $.extend({
+				href 		:null,
+				text		:null,
+				target	:'_self'
+			}, options);
+
+			// Validation
+			if(settings.href == null){
+				console.log('You need an href option for LinkMe to work.');
+				return this;
+			}
+
+			return this.each(function() {
+				var object = $(this);
+
+				if(settings.text == null) {
+					settings.text = object.text();
+
+				}
+
+				object.wrap('<a target="'+settings.target+'" href="'+settings.href+'"></a>').text(settings.text) ;
+
+			});
+
+
+		}
+
+	}(jQuery));
